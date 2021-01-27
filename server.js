26294ebcabd9f9d104536abf20fe3536bda7b6ef
sameghosts <seanfeiner.brown@gmail.com>
2021-01-27 14:34:35 -0800

    first branch commit

--- server.js
+++ server.js
@@ -66,3 +66,6 @@ app.use('/auth', require('./routes/auth'));
 var server = app.listen(process.env.PORT || 3000, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
 
 module.exports = server;
+
+//heres a comment to test branches
+
